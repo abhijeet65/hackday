ef97dd5f7980d541f64585b2273aab85fbc9cc18
priya <laxmisuprini@gmail.com>
2018-12-01 11:48:14 +0530

    sorted

--- hack.py
+++ hack.py
@@ -1,14 +1,33 @@
 import csv
+import matplotlib.pyplot as plt
+import operator
+x = []
+y = []
 
 with open("jeans.csv") as csv_file:
     csv_reader = csv.reader(csv_file, delimiter=',')
     line_count = 0
-    for row in csv_reader:
+    sortedlist = sorted(csv_reader, key=operator.itemgetter(3),reverse=True)
+    for row in sortedlist:
+    	if(line_count<=11 and line_count>0):
+    		print(row[0]+ " "+row[1] +" "+ row[2] +" "+row[3])
+    	line_count += 1
+        		
+
+
+  
+'''    for row in csv_reader:
         if line_count == 0:
             print('Column names are {", ".join(row)}')
             line_count += 1
         else:
-            print(row[0]+ " "+row[1] +" "+ row[2] +" "+row[3])
+            if (line_count <= 10) :
+            	#print(row[0]+ " "+row[1] +" "+ row[2] +" "+row[3])
             line_count += 1
-			
-    print('Processed {line_count} lines.')
+            x.append((row[2]))
+            y.append((row[3]))
+    print(a)
+'''    
+    
+
+    
